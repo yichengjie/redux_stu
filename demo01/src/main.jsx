1f474ad3412af03a165617027d6947937171e550
yichengjie <626659321@qq.com>
2016-07-22 11:19:19 +0800

    'demo1'

--- demo01/src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import Notes from './Notes.jsx' ;
-ReactDom.render(<Notes/>,
-	document.getElementById('app')
-) ;
--- demo01/webpack.config.js
+++ demo01/webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: './src/main.jsx',
+    entry: './src/main.js',
     output: {
         path: __dirname+"/dist",
         filename: 'bundle.js',
