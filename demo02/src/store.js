5041ab625291dd5c33f4af798d39a07639bc0455
yichengjie <626659321@qq.com>
2016-07-31 17:22:43 +0800

    commit

--- demo02/src/store.js
+++ demo02/src/store.js
@@ -2,7 +2,11 @@
 import { combineReducers, createStore } from 'redux' ;
 import visibilityFilter from './reducers/visibilityFilter.js' ;
 import todos from './reducers/todos.js' ;
-let reducer = combineReducers({ visibilityFilter, todos }) ;
+let reducer = combineReducers({
+	 visibilityFilter,
+	 todos 
+	}) ;
+
 let store = createStore(reducer) ;
 
 export {store} ;
