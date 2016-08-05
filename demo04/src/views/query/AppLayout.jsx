5066d9d91da5adeb5b5882a6a2161ace792132e0
yichengjie <626659321@qq.com>
2016-08-05 10:29:32 +0800

    demo without redux is finish

--- demo04/src/views/query/AppLayout.jsx
+++ demo04/src/views/query/AppLayout.jsx
@@ -4,40 +4,47 @@ import QuerySection from './QuerySection.jsx';
 import OperNavBar from './OperNavBar.jsx' ;
 import BrandGroupPanel from './BrandGroupPanel.jsx' ;
 
+let listData = [{
+    id:'001',title:'asgui_ca test',seqNum:'8776559',startCity:'1',endCity:'2',
+    startDate:'2016-06-30',endDate:'9999-12-31',travelStart:'2016-06-30',
+    travelEnd:'2016-06-30',pubObj:'001',stateDesc:'未发布',list:[{
+        id:"001",
+        name:'xx001',
+        levelNum:'10011',
+        desc:'测试一下啊这里的描述信息'
+      },
+      {
+        id:"002",
+        name:'xx002',
+        levelNum:'10013',
+        desc:'测试一下啊这里的描述信息'
+      },{
+        id:"003",
+        name:'xx003',
+        levelNum:'10013',
+        desc:'测试一下啊这里的描述信息'
+      }
+    ]
+  }
+];
 
 class AppLayout extends Component {
   constructor(props) {
     super(props) ;
-    this.state = {list: [{
-        id:'001',title:'asgui_ca test',seqNum:'8776559',startCity:'1',endCity:'2',
-        startDate:'2016-06-30',endDate:'9999-12-31',travelStart:'2016-06-30',
-        travelEnd:'2016-06-30',pubObj:'001',stateDesc:'未发布',list:[{
-            id:"001",
-            name:'xx001',
-            levelNum:'10011',
-            desc:'测试一下啊这里的描述信息'
-          },
-          {
-            id:"002",
-            name:'xx002',
-            levelNum:'10013',
-            desc:'测试一下啊这里的描述信息'
-          },{
-            id:"003",
-            name:'xx003',
-            levelNum:'10013',
-            desc:'测试一下啊这里的描述信息'
-          }
-        ]
-      }
-    ]};
+    this.state = {list:[] };
+  }
+  handleQueryBrand(param){
+    console.info(JSON.stringify(param)) ;
+    setTimeout(function(){
+      this.setState({list:listData}) ;
+    }.bind(this),300) ;
   }
   render(){
     return (
       <div>
           <div className="navbar-fixed-top" id = "myheader">
               <HeaderNav/>
-              <QuerySection/>
+              <QuerySection handleQueryBrand = {this.handleQueryBrand.bind(this)}/>
           </div>
           <div className="container-fluid main_content" id="main_content" >
               <OperNavBar/>
