1b8f000f7b03ee0fd5755133b0c2bde0da4ee52a
yichengjie <626659321@qq.com>
2016-08-08 17:46:16 +0800

    commit

--- demo06/webpack.config.js
+++ demo06/webpack.config.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var LIB_PATH = path.resolve('./lib');
 
 module.exports = {
     entry: {
@@ -13,6 +14,20 @@ module.exports = {
         publicPath:'./dist'
     },
     devtool: 'eval-source-map',
+    resolve: {
+        alias: {
+            tui_core_lib:LIB_PATH+"/tui-core/index.js",
+            tui_dialog_lib:LIB_PATH+"/tui-dialog/index.js",
+            tui_drag_lib:LIB_PATH+"/tui-drag/index.js",
+            jq_datepicker_lib: LIB_PATH+"/jq-datepicker/index.js",
+            jq_timepicker_lib:LIB_PATH+"/jq-timepicker/index.js",
+            jq_validate_lib:LIB_PATH+"/jq-validate/index.js",
+            bt_growl_lib:LIB_PATH+"/bt-grow/jquery.bootstrap-growl.js",
+            is_loading_lib:LIB_PATH+"/is-loading/index.js",
+            moment_lib:LIB_PATH+"/moment/index.js",
+            util_lib:LIB_PATH+"/util.js"
+        }
+    },
     module: {
         preLoaders: [
           {
@@ -37,7 +52,8 @@ module.exports = {
     plugins: [
         new webpack.BannerPlugin('{compony travelsky-dbky ,\n author: yicj,\n email : 626659321@qq.com,\ncreate-date:2016/05/05}')
         ,new webpack.ProvidePlugin({
-          $: 'jquery'
+          $: 'jquery',
+          jQuery: 'jquery'
         })//这个可以使jquery变成全局变量，妮不用在自己文件require('jquery')了
         //new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js')//这是妮第三方库打包生成的文件
         ,new webpack.optimize.CommonsChunkPlugin({
