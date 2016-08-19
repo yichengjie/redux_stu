df09e9ccb80a732b29f65fc34639c3f0f02b13d4
yichengjie <626659321@qq.com>
2016-08-19 10:46:12 +0800

    t

---
+++ demo08/src/demo02.jsx
@@ -0,0 +1,30 @@
+import {Form,FormGroup,ControlLabel,InputGroup} from 'react-bootstrap'; 
+
+import React,{Component} from 'react'; 
+export default class App extends Component {
+    constructor(props){
+        super(props) ;
+    }
+    render(){
+        return(
+            <Form componentClass="fieldset" inline>
+                <FormGroup controlId="formValidationWarning4" validationState="warning">
+                    <ControlLabel>Input with warning</ControlLabel>
+                    {' '}
+                    <FormControl type="text" />
+                    <FormControl.Feedback />
+                </FormGroup>
+                {' '}
+                <FormGroup controlId="formValidationError4" validationState="error">
+                    <ControlLabel>Input group with error</ControlLabel>
+                    {' '}
+                    <InputGroup>
+                    <InputGroup.Addon>@</InputGroup.Addon>
+                    <FormControl type="text" />
+                    </InputGroup>
+                    <FormControl.Feedback />
+                </FormGroup>
+            </Form>
+        ) ;
+    }
+}
