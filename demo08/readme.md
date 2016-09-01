590fcd6c06afd0e1b65f59000a9903a5aad0c2f6
yichengjie <626659321@qq.com>
2016-09-01 17:16:03 +0800

    增加各个测试demo的说明文件

---
+++ demo08/readme.md
@@ -0,0 +1,1 @@
+# 测试 react-bootstrap的Form
--- demo08/src/main.js
+++ demo08/src/main.js
@@ -2,6 +2,7 @@ import React from 'react' ;
 import ReactDOM,{render} from 'react-dom' ; 
 //import App from './demo01.jsx' ;
 import App from './demo02.jsx' ;
+
 render(
     <App/>,
     document.getElementById('app')
